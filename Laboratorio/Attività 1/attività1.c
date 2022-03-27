624f3ff838ab15f6ad0c2d0dc258e151c58609ad
Francesco Passiatore <94631872+mrKpax@users.noreply.github.com>
2022-03-27 23:43:21 +0200

    Create attività1.c

---
+++ Laboratorio/Attività 1/attività1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "modulo.h"
+#define MAX 50
+
+char minimo(char *s)
+{ 
+    char min = s[0];
+    int len = strlen(s);
+    for (int i = 1; i < len; i++)
+    {
+        if (s[i] < min)
+        {
+            min = s[i];
+        }
+    }
+    return min;
+}
+
+int main()
+{
+    char *nome, *cognome, minn, minc;
+
+    nome = xcalloc(MAX, sizeof(char));
+    cognome = xcalloc(MAX, sizeof(char));
+
+    printf("Inserisci il nome: ");
+    scanf("%s", nome);
+    nome = xrealloc(nome, strlen(nome)+1);
+    
+    printf("Inserisci il cognome: ");
+    scanf("%s", cognome);
+    cognome = xrealloc(cognome, strlen(cognome)+1);
+
+    minn = minimo(nome);
+    minc = minimo(cognome);
+    printf("Minimo del nome: %c, minimo del cognome: %c\n", minn, minc);
+
+    return 0;
+}
