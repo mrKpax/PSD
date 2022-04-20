5a14b6fc71324c04066594d5f0815a9fadbd79cf
Francesco Passiatore <94631872+mrKpax@users.noreply.github.com>
2022-04-20 16:35:56 +0200

    Create linea_comando.c

---
+++ Introduzione/linea_comando.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "vettore.h"
+#include "utile.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        printf("Numero di elementi da ordinare insufficienti\n");
+    }
+    else
+    {
+        int n = atoi(argv[1]);
+        int *a = xcalloc(n,sizeof(int));
+
+        input_array(a,n);
+        selection_sort(a,n);
+        output_array(a,n);
+    }
+}
