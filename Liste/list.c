68481c215ee970db7d2b836a29a9c6374bcba866
Francesco Passiatore <94631872+mrKpax@users.noreply.github.com>
2022-05-05 12:37:52 +0200

    Update list.c

--- Liste/list.c
+++ Liste/list.c
@@ -57,3 +57,138 @@ item getFirst(list l)
     }
     return e;
 }
+
+int sizeList(list l)
+{
+    int n = 0;
+    while (!emptyList(l)) //finchè non raggiungiamo la fine della lista, visita totale
+    {
+        n++; //il primo elemento contribuisce al conteggio
+        l = tailList(l); //visita degli elementi successivi
+    }
+    return n;
+}
+
+int posItem(list l, item val)
+{
+    int pos = 0;
+    int found = 0;
+
+    while (!emptyList(l) && !found)
+    {
+        if (eq(getFirst(l), val))
+        {
+            found = 1;
+        }
+        else
+        {
+            pos++;
+            l = tailList(l);
+        }
+    }
+
+    if (!found)
+    {
+        pos = -1;
+    }
+
+    return pos;
+}
+
+/* VERSIONE RICORSIVA di posItem
+int posItem(list l, item val)
+{
+    if emptyList(l) return -1;
+    if (eq(getFirst(l), val)) return 0;
+    else
+    {
+        int ris = posItem(tailList(l), val);
+        if (ris == -1)
+            return -1;
+        else
+            return 1 + ris;
+    }
+} */
+
+getItem(list l, int pos)
+{
+    item e;
+    int i = 0;
+
+    while (i<pos && !emptyList(l)) //scorriamo la lista fino a pos (se esiste)
+    {
+        i++;
+        l = tailList(l);
+    }
+
+    if (!emptyList(l)) //se la lista ha almeno pos+1 elementi
+        e = getFirst(l); //elemento di posizione pos
+    else
+        e = NULLITEM;
+
+    return e;
+}
+
+list reverseList(list l) //utilizzo tipico lst2 = reverseList(lst1)
+{
+    list rev;
+    item val;
+
+    rev = newList();
+
+    while (!emptyList(l)) //visita totale
+    {
+        val = getFirst(l);
+        rev = consList(val, rev);
+        l = tailList(l);
+    }
+    return rev;
+}
+
+outputList(list l)
+{
+    int i = 0;
+    item val;
+
+    while(!emptyList(l)) //visita totale
+    {
+        val = getFirst(l);
+        printf("Elemento di posizione %d: ",i);
+        output_item(val);
+        printf("\n");
+        l = tailList(l);
+        i++;
+    }
+}
+
+/* VERSIONE RICORSIVA di outpuList
+void outputLis(list l)
+{
+    out_List(l, 0);
+}
+
+void out_List(list l, int i)
+{
+    if (emptyList(l)) return;
+    
+    val = getFirst(l);
+    printf("Elemento di posizione %d: ",i);
+    output_item(val);
+    printf("\n");
+    out_List(tailList(l), i+1);
+} */
+
+list inputList(int n)
+{
+    item val;
+    list l = newList();
+
+    for(int i=0; i<n; i++)
+    {
+        printf("Elemento di posizione %d: ", i);
+        input_item(&val);
+        l = consList(val, l);
+    }
+
+    return reverseList(l); //poichè alla fine del ciclo l contiene la lista al contrario
+}
