9fd86f4496abf6c7c6740fe2c2e0aabb3b3def4a
Francesco Passiatore <94631872+mrKpax@users.noreply.github.com>
2022-05-12 12:20:13 +0200

    Create stacklist.c

---
+++ Stack/stacklist.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "item.h"
+#include "stack.h"
+
+struct node {
+    item value;
+    struct node *next;
+};
+
+struct c_stack {
+    struct node *top;
+    int numel;
+}
+
+stack newStack(void)
+{
+    stack s;
+    s = xmalloc(sizeof(struct c_stack));
+    s->numel = 0;
+    s->top = NULL;
+    return s;
+}
+
+int emptyStack(stack s)
+{
+    return s->numel == 0;
+}
+
+int push(item val, stack s)
+{
+    struct node *nuovo;
+    nuovo = xmalloc(sizeof(struct node));
+
+    nuovo->value = val;
+    nuovo->next = s->top;
+    s->top = nuovo;
+    (s->numel)++;
+
+    return 1;
+}
+
+int pop(stack s)
+{
+    if (s->numel == 0)
+    {
+        return 0;
+    }
+
+    struct node *temp;
+    temp = s->top;
+    s->top = s->top->next;
+    free(temp);
+    (s->numel)--;
+
+    return 1;
+}
+
+item top(stack s)
+{
+    item e;
+    if (s->numel > 0)
+    {
+        e = s->top->value;
+    }
+    else
+    {
+        e = NULLITEM;
+    }
+    return e;
+}
