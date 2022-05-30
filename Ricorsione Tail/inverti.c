b3c93c0b42f17c44110319f4c40f1f71e3964ca7
Francesco Passiatore <94631872+mrKpax@users.noreply.github.com>
2022-05-30 14:21:51 +0200

    Update inverti.c

--- Ricorsione Tail/inverti.c
+++ Ricorsione Tail/inverti.c
@@ -1,11 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
 void print_rev (char car)
 {
   char c;
   if (car != '.')
   {
     c = getchar();
+    printf("lettera %c\n", c);
     print_rev(c); //ogni record di attivazione nello stack memorizza un singolo carattere letto (push); in fase di pop, i caratteri vengono stampati nell'ordine inverso
     printf("%c", car);
   }
   else return;
+}
+
+main()
+{
+  char k;
+  printf("\nIntrodurre una sequenza terminata da . : \t ");
+  k = getchar();
+  print_rev(k);
+  printf("\n FINE \n");
 }
